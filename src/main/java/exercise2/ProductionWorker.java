6b2475346ba0fa734cd13d7b1ed83781ffbf0f7a
JunxianXie <xie.junxian@yahoo.com>
2017-10-20 16:22:43 +0200

    Exercice 2 fini

--- src/main/java/exercise2/ProductionWorker.java
+++ src/main/java/exercise2/ProductionWorker.java
@@ -1,11 +1,18 @@
 package exercise2;
 
-public class ProductionWorker {
-	public void produceOnePiece() {
+public class ProductionWorker implements Payable {
+	private int salaryperpiece;
+	private int piece=0;
+	public ProductionWorker (int salaryperpiece){
 
+		this.salaryperpiece=salaryperpiece;
+
+	}
+	public void produceOnePiece() {
+		piece++;
 	}
 
 	public int computeSalary() {
-		return 0;
+		return salaryperpiece*piece;
 	}
 }
